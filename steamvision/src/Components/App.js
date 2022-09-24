5a846217dcf554fae91e336cc77177c5beb6fe68
physicalrobot <kimboho@gmail.com>
2022-09-24 13:43:36 -0700

    started building website

---
+++ steamvision/src/Components/App.js
@@ -0,0 +1,15 @@
+import logo from '../logo.svg';
+import '../Styles/App.css';
+import Homepage from './Homepage'
+import Header from './Header'
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Homepage />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ steamvision/src/Components/Homepage.js
@@ -0,0 +1,15 @@
+
+
+function Homepage() {
+
+
+    return (
+        <h1>Hello</h1>
+    )
+
+
+
+}
+
+
+export default Homepage
